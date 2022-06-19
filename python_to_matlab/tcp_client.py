7ee214e3f32c1ea99f050bf87837189e47a3bca8
NFUEEchris <74455348+NFUEEchris@users.noreply.github.com>
2022-06-19 13:04:54 +0800

    Add files via upload

--- python_to_matlab/tcp_client.py
+++ python_to_matlab/tcp_client.py
@@ -4,12 +4,11 @@ import socket
 import numpy as np
 import cv2
 import base64
-sendmessage = 'hello'
+file_location = "C:\\Users\\bblow\\AppData\\Local\\Programs\\Python\\Python310\\tcp"
+img_name = "222.jpg"
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('192.168.11.5', 54378))
-
-with open("C:\\Users\\bblow\\AppData\\Local\\Programs\\Python\\Python310\\tcp\\222.jpg", "rb") as img_file:
-
+with open(file_location+"\\"+img_name, "rb") as img_file:
     data = base64.b64encode(img_file.read())
 print(len(data))
 sock.sendall(data)
--- python_to_matlab/tcp_sever.m
+++ python_to_matlab/tcp_sever.m
@@ -1,38 +1,37 @@
 clc
 clear
 
-initialfile="C:\Users\bblow\AppData\Local\Programs\Python\Python310\tcp";   %initial file
+targetfile="C:\Users\bblow\AppData\Local\Programs\Python\Python310\tcp";   %target file
+targetname="matlab_receive_result.jpg"; %output img name
 myip='192.168.11.5';        %your local ip
+
+
 tcpservo = tcpserver(myip, 54378);
 counter=0;
 totalstr={''};
 while 1
-    if tcpservo.NumBytesAvailable>0
-        counter=counter+1;
-        recv=read(tcpservo,65536,"char");
-        strstore{counter}=recv;
-        
-        if length(recv)<65536
-            for i=1:length(strstore)
-                totalstr=append(totalstr,strstore{i});
-            end
 
-            fprintf("length:%d\n",strlength(string(totalstr)));
-            counter=0;
-            raw = matlab.net.base64decode(string(totalstr));
-            fid = fopen("C:\Users\bblow\AppData\Local\Programs\Python\Python310\tcp\tcp_result.jpg",'wb');
-            fwrite(fid,raw,'uint8');            
-            fclose(fid);
-            break;
+    counter=counter+1;
+    recv=read(tcpservo,65536,"char");
+    strstore{counter}=recv;
+    if isempty(recv)
+        fprintf("fail..");
+        break;
+    end
+    if length(recv)<65536
+        for i=1:length(strstore)
+            totalstr=append(totalstr,strstore{i});
         end
-%         fprintf(string(recv));
-%         encoder = org.apache.commons.codec.binary.Base64;
-%         bytes = encoder.decode(uint8(recv'));
-%         fopen('C:\\Users\\bblow\\AppData\\Local\\Programs\\Python\\Python310\\tcp\\aaaa.jpg','wb');
-%         write(bytes);
-%         fclose('C:\\Users\\bblow\\AppData\\Local\\Programs\\Python\\Python310\\tcp\\aaaa.jpg');
-%         break;
+        fprintf("length:%d\n",strlength(string(totalstr)));
+        
+        counter=0;
+        raw = matlab.net.base64decode(string(totalstr));
+        fid = fopen(strcat(strcat(targetfile,"\"),targetname),'wb');
+        fwrite(fid,raw,'uint8');            
+        fclose(fid);
+        break;
     end
 end
-f=imread("C:\Users\bblow\AppData\Local\Programs\Python\Python310\tcp\tcp_result.jpg");
+delete(tcpservo)
+f=imread(strcat(strcat(targetfile,"\"),targetname));
 imshow(f);
